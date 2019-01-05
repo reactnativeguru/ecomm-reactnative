973cbc6f04784a6c803e9ab10fec56478980bed7
John <john.amuesi@wipro.com>
2019-01-05 11:19:32 +0000

    completed image selection functionality

--- ecommapp/src/components/ImageBrowser.js
+++ ecommapp/src/components/ImageBrowser.js
@@ -1,41 +1,50 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
   CameraRoll,
   Image,
   StyleSheet,
   TouchableHighlight,
-  View, Text
-} from 'react-native';
+  View,
+  Text
+} from 'react-native'
+import ViewPhotos from './ViewPhotos'
 
 class Camera extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showPhotoGallery: '',
+      photoArray: []
+    }
+  }
 
-    getPhotos = () => {
-  CameraRoll.getPhotos({
-    first: 20,
-    assetType: 'All'
-  })
-  .then(r => this.setState({ photos: r.edges }))
-}
-  
+  getPhotos = () => {
+    CameraRoll.getPhotos({
+      first: 20
+    }).then(res => this.setState({ 
+        showPhotoGallery: true, 
+        photoArray: res.edges }))
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-        <TouchableHighlight
-        onPress={this.getPhotos}
-        >
-         <Text>Add Image</Text>
+  render() {
+    if (this.state.showPhotoGallery) {
+      return <ViewPhotos photoArray={this.state.photoArray} />
+    }
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={() => this.getPhotos()}>
+          <Text>Add Image</Text>
         </TouchableHighlight>
       </View>
-        );
-    }
+    )
+  }
 }
-export default Camera;
+export default Camera
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-});
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
